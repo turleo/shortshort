20131486a4bff4ba908b63302dceaa04f0678371
turleo <turejsijleonid004@gmail.com>
2020-07-03 12:48:11 +1000

    Added copy button

--- static/short.js
+++ static/short.js
@@ -1,3 +1,12 @@
+function onload(){
+    if (!navigator.share) {
+        document.getElementById("share").style.display = "none";
+    }
+    if (!navigator.clipboard) {
+        document.getElementById("share").style.display = "none";
+    }
+}
+
 function short(){
     var out = document.getElementById("out");
     var text = document.getElementById("link").value;
@@ -8,7 +17,20 @@ function short(){
         var url = window.location.href + xhr.response;
         out.innerHTML = url;
         out.href = url;
+        document.getElementById("res").style.display = "block";
     };
 
     console.log(text);
+}
+
+
+function share(){
+    document.getElementById("out")
+}
+
+
+function copy(){
+    var blob = new Blob([document.getElementById("out").innerHTML], {type: 'text/plain'});
+    var item = new ClipboardItem({'text/plain': blob});
+    navigator.clipboard.write([item])
 }
