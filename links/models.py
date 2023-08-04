fc1fca699a34fb37d2988114561fca192cd98ad4
Valya Tureyskaya <turleo@outlook.com>
2023-08-04 14:01:19 +1000

    🚨 Fixed django model warning

--- links/models.py
+++ links/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from datetime import datetime
 
 # Create your models here.
 class Link(models.Model):
-	short = models.CharField(max_length=50, default=datetime.now())
+	id = models.AutoField(primary_key=True)
+	short = models.CharField(max_length=50)
 	long = models.TextField()
 	publsh = models.DateTimeField(auto_now=True)
